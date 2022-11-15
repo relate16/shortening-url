394f97c2db4e6fe6ad38b91f16cb860bc472e7ce
relate16 <relate16@naver.com>
2022-11-15 12:40:56 +0900

    first commit/ 초기 환경 세팅 완료
    애플리케이션 정상 작동 확인(test포함)
    h2 DB연결 및 테이블 생성 확인
    LOMBOK 정상 작동 확인

---
+++ src/main/java/com/example/shorteningurl/ShorteningUrlApplication.java
@@ -0,0 +1,15 @@
+package com.example.shorteningurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.shorteningurl.entity.Test;
+
+@SpringBootApplication
+public class ShorteningUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShorteningUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shorteningurl/ShorteningUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shorteningurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorteningUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
